ac65f510a7e1917c2fa2e0f4ac2b40c8bc770561
dongamp1990 <dongamp1990@gmail.com>
2017-11-20 14:24:38 +0800

    提交

--- ali-centos7.sh
+++ ali-centos7.sh
@@ -2,5 +2,3 @@
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 mv /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel.repo.backup
-mv /etc/yum.repos.d/epel-testing.repo /etc/yum.repos.d/epel-testing.repo.backup
-wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
